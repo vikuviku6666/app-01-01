74efd6593e1b4416f11846ba91b7ebad6a4e225d
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2024-02-26 20:40:12 +0100

    first commit

---
+++ ClassLibrary1/WeatherCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassLibrary1;
+
+public class WeatherCalculator
+{
+public static string DetermineSeason(DateOnly date)
+{
+    int month = date.Month;
+
+    if (month >= 3 && month <= 5)
+    {
+      return "Spring";
+    }
+    if (month >= 6 && month <= 8)
+    {
+      return "Summer";
+    }
+    if (month >= 9 && month <= 11)
+    {
+      return "Autumn";
+    }
+    return "Winter";
+}
+}
