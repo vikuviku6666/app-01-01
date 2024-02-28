6d0e948d36813aecfa0caf174398173b81e14cdc
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2024-02-28 16:36:42 +0100

    initial commit

--- ClassLibrary1/obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Debug/net8.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74efd6593e1b4416f11846ba91b7ebad6a4e225d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
